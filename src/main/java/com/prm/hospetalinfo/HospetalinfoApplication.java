4539ae590245439f2a95300bc8565bf09da2c93f
Venkat Venky <venkateshvenky506@gmail.com>
2018-11-04 11:17:18 +0530

    hello

---
+++ src/main/java/com/prm/hospetalinfo/HospetalinfoApplication.java
@@ -0,0 +1,12 @@
+package com.prm.hospetalinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospetalinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospetalinfoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/prm/hospetalinfo/HospetalinfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prm.hospetalinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HospetalinfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
